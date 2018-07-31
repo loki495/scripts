ab91912a3987387be0a9446d1128eb90eab75124
Andres Crucitti <dasc495@gmail.com>
2018-07-30 17:28:02 -0700

    Better search for beta/latest tag release

--- media_scripts/emby/update-emby.php
+++ media_scripts/emby/update-emby.php
@@ -1,18 +1,29 @@
 <?php
-$cmd = "curl -s https://api.github.com/repos/MediaBrowser/Emby.Releases/releases";
+$cmd = "curl -s https://api.github.com/repos/MediaBrowser/Emby.Releases/tags";
 $json = json_decode(shell_exec($cmd));
-$assets = $json[0]->assets;
+$latest_tag = $json[0]->name;
+
+//$cmd = "curl -s https://api.github.com/repos/MediaBrowser/Emby.Releases/releases";
+$cmd = "curl -s https://api.github.com/repos/MediaBrowser/Emby.Releases/releases/tags/$latest_tag";
+$json = json_decode(shell_exec($cmd));
+$assets = $json->assets;
 
 $url = "";
 foreach ($assets as $asset) {
     $name = $asset->name;
+    if (stripos($name,"emby-server-deb") !== 0) {
+        continue;
+    }
     if (stripos($name,"amd64.deb") === FALSE) {
         continue;
     }
+    /*
     if (stripos($name,"netgear") !== FALSE) {
         continue;
     }
+    */
     $url = $asset->browser_download_url;
+    break;
 }
 
 if (!$url) die("NO DEB 64-bit FILE FOUND!\n"); else echo "Found release: $url\n";
